6fd63b17dc298c6698e0c74d9f58457c0111de28
Adrien Ferré <ferre.adrien@gmail.com>
2011-05-29 22:53:50 +0200

    Initial commit of demo project

---
+++ afGMDemo/afGMDemo/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  afGMDemo
+//
+//  Created by adrien ferré on 29/05/11.
+//  Copyright 2011 Ferré. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ afGMDemo/afGMDemo/afGMDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  afGMDemoAppDelegate.h
+//  afGMDemo
+//
+//  Created by adrien ferré on 29/05/11.
+//  Copyright 2011 Ferré. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface afGMDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
