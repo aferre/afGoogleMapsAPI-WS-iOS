3cecd1a0e1c038bf1f9d8d5dec5f04e5c07bd77a
Adrien Ferré <ferre.adrien@gmail.com>
2011-06-02 18:37:31 +0200

    Added Places API

---
+++ iOS/afGMapsPlacesRequest.m
@@ -0,0 +1,16 @@
+//
+//  afGMapsPlacesRequest.m
+//  afGMDemo
+//
+//  Created by adrien ferré on 01/06/11.
+//  Copyright 2011 Ferré. All rights reserved.
+//
+
+#import "afGMapsPlacesRequest.h"
+
+
+@implementation afGMapsPlacesRequest
+
+@synthesize afDelegate;
+
+@end
