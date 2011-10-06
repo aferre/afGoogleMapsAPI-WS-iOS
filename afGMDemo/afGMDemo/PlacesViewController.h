aa05cc638ee6a1c2f573cc843893336d0b4e8a2e
Ferre Adrien <ferre.adrien@gmail.com>
2011-10-06 19:41:54 +0200

    Added PlacesViewController

---
+++ afGMDemo/afGMDemo/PlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacesViewController.h
+//  afGMDemo
+//
+//  Created by adrien ferré on 06/10/11.
+//  Copyright 2011 Ferré. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesViewController : UIViewController
+
+@end
